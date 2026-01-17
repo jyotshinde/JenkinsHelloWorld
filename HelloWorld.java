09ab207799788c6b799c891a3ea6b02d5372e01c
Jyotiram Shinde <228212909+jyotshinde@users.noreply.github.com>
2026-01-17 19:26:38 +0530

    webhook

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args){
-    System.out.println("---Welcom to the first jenkins page---");
+    System.out.println("---Webhook---");
   }
 }
+
